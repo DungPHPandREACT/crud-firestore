49fb290330e05b2b370835608cd19ae54299c44d
DungPHPandREACT <dung2000cn@gmail.com>
2022-04-21 20:06:48 +0700

    update and delete firestore

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import {
   addDoc,
   getDocs,
   getFirestore,
+  deleteDoc,
+  doc,
+  updateDoc,
 } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js';
 import { app } from './config.js';
 
@@ -39,3 +42,20 @@ querySnapshot.forEach((doc) => {
 });
 
 document.getElementById('content-posts').innerHTML = stringHTML;
+
+document.getElementById('delete').onclick = async function () {
+  const response = await deleteDoc(doc(db, 'posts', '23RhSVbLr6GmGHScqj2G'));
+  console.log(response);
+};
+
+document.getElementById('update').onclick = async function () {
+  const postCollection = doc(db, 'posts', '3ebgcbVumNFvpr2dV5Vc');
+
+  const newPost = {
+    email: 'testupdate',
+    password: 'testupdate',
+  };
+  // Set the "capital" field of the city 'DC'
+  const response = await updateDoc(postCollection, newPost);
+  console.log(response);
+};
